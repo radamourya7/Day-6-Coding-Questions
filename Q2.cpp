de39d3387e955b17db3eecafbc427d8ec3f2952f
Mourya Rada <145229013+radamourya7@users.noreply.github.com>
2024-08-27 01:23:25 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -10,3 +10,28 @@ Sum of even numbers: 30
 
 CODE:=
 
+#include<iostream>
+using namespace std;
+int main(){
+    int odd=0;
+    int even=0;
+    int size;
+    cout<<"Enter the size of the array: ";
+    cin>>size;
+    int arr[size];
+    cout<<"Enter the array: ";
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    for(int j=0;j<size;j++){
+        if(arr[j]%2==0){
+            even=even+arr[j];
+        }
+        else{
+            odd=odd+arr[j];
+        }
+    }
+    cout<<"Sum of odd numbers: "<<odd<<endl;
+    cout<<"Sum of even numbers: "<<even;
+    return 0;
+}
